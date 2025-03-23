b8fc68051e05f79d66e851e903b63207db3627b8
tanselberkant <tanselberkant@gmail.com>
2025-03-23 05:39:39 +0300

    ready to release

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import Tooltip, {
+  TooltipProps,
+  TooltipPosition,
+  TooltipColor,
+} from "./components/Tooltip";
+
+export type { TooltipProps, TooltipPosition, TooltipColor };
+export default Tooltip;
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "TailTooltipReact",
+      fileName: (format) => `tail-tooltip-react.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
 });
